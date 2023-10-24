6bc7c5ffde3a9100c2e1e8e898daca5eb1f9e82b
Chunyang Shen <scy0208@gmail.com>
2023-10-23 17:26:37 -0700

    Add support page

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/cn',
-        permanent: true,
-      },
-    ]
-  },
     async headers() {
         return [
           {
